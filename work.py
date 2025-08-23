53174097e6742933add2d24ecff1be8e656f1c43
TheArjunAgarwal <anulick0@gmail.com>
2025-08-24 03:52:02 +0530

    new page

---
+++ work.py
@@ -0,0 +1,16 @@
+
+def work(filename):
+    with open(filename, "r", encoding="utf-8") as f:
+        lines = f.read().splitlines()
+        i = 0
+        ans = []
+        for line in lines:
+            if i % 2 == 0:
+                ans.append(line)
+            i += 1
+    return ans
+
+if __name__ == "__main__":
+    filename = "data.txt"  # change to your file name
+    lines = work(filename)
+    print(lines)
